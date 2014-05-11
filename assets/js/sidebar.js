2c3352a969a0b4eaf28757a6eafbb4fd1f9e3a96
Blake Parkinson <blake@edmodo.com>
2014-05-10 22:45:34 -0700

    Create sidebar.js

---
+++ assets/js/sidebar.js
@@ -0,0 +1,16 @@
+var Sidebar = Backbone.Model.extend({
+  promptColor: function() {
+    var cssColor = prompt("Please enter a CSS color:");
+    this.set({color: cssColor});
+  }
+});
+
+window.sidebar = new Sidebar;
+
+sidebar.on('change:color', function(model, color) {
+  $('#sidebar').css({background: color});
+});
+
+sidebar.set({color: 'white'});
+
+sidebar.promptColor();
